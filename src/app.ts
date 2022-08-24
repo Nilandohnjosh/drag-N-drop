3b97446a29a4dd84c25f4d550ec3c4a98ca0ad6e
Nilandohnjosh <joshngaoluangraj@gmail.com>
2022-08-24 14:16:46 +1200

    can fetch user input, with class validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,47 @@
+// Validation
+interface Validatable {
+  value: string | number
+  required?: boolean
+  minLength?: number
+  maxLength?: number
+  min?: number
+  max?: number
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max
+  }
+  return isValid
+}
+
 // autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value
@@ -32,13 +76,13 @@ class ProjectInput {
 
     this.titleInputElement = this.element.querySelector(
       '#title'
-    )! as HTMLInputElement
+    ) as HTMLInputElement
     this.descriptionInputElement = this.element.querySelector(
       '#description'
-    )! as HTMLInputElement
+    ) as HTMLInputElement
     this.peopleInputElement = this.element.querySelector(
       '#people'
-    )! as HTMLInputElement
+    ) as HTMLInputElement
 
     this.configure()
     this.attach()
@@ -49,10 +93,26 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value
     const enteredPeople = this.peopleInputElement.value
 
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    }
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    }
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    }
+
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredPeople.trim().length === 0
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
     ) {
       alert('Invalid input, please try again!')
       return
@@ -72,8 +132,8 @@ class ProjectInput {
     event.preventDefault()
     const userInput = this.gatherUserInput()
     if (Array.isArray(userInput)) {
-      const [title, description, people] = userInput
-      console.log(title, description, people)
+      const [title, desc, people] = userInput
+      console.log(title, desc, people)
       this.clearInputs()
     }
   }
